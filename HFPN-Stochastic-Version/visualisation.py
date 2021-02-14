9aa76788b61e0bd606a4ca14956007d7582668dd
BrandonSLockey <72495991+BrandonSLockey@users.noreply.github.com>
2021-02-14 17:22:38 +0000

    Delay List for MDVs

--- HFPN-Stochastic-Version/visualisation.py
+++ HFPN-Stochastic-Version/visualisation.py
@@ -35,6 +35,7 @@ class Analysis:
         self.delay_list_t_A = hfpn.transitions['t_A'].delay_list 
         self.delay_list_t_B = hfpn.transitions['t_B'].delay_list 
         self.delay_list_t_D = hfpn.transitions['t_D'].delay_list 
+        self.delay_list_MDVs = hfpn.transitions['t_MDV_Generation_basal'].delay_list
         self.df_for_rate_analytics_prod = hfpn.df_for_rate_analytics_prod # we will access all the fired tokens from this in plotting later
         self.df_for_rate_analytics_cons=hfpn.df_for_rate_analytics_cons
         
