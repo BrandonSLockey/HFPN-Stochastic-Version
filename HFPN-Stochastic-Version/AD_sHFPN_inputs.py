21e7a22a83f7f2a401d3c4a93fdeb18c9595a179
BrandonSLockey <72495991+BrandonSLockey@users.noreply.github.com>
2021-03-24 13:13:12 +0000

    SD

--- HFPN-Stochastic-Version/AD_sHFPN_inputs.py
+++ HFPN-Stochastic-Version/AD_sHFPN_inputs.py
@@ -6,9 +6,9 @@ time_step_size = 0.01
 
 #To run model without Stochasticity, Set SD to 0.
 
-cholSD = 0.5 # cholesterol module
-SD = 0.5
-DelaySD = 0
+cholSD = 0.1 # cholesterol module
+SD = 0.1
+DelaySD = 0.1
 
 
 
