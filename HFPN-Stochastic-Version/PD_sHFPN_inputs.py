24ebafc04039f9c4983518c915c1701b191d5006
BrandonSLockey <72495991+BrandonSLockey@users.noreply.github.com>
2021-03-24 13:13:37 +0000

    inputs pd

--- HFPN-Stochastic-Version/PD_sHFPN_inputs.py
+++ HFPN-Stochastic-Version/PD_sHFPN_inputs.py
@@ -1,13 +1,13 @@
 #MUTATIONS (BINARY)
-it_p_GBA1 = 0
-it_p_LRRK2_mut = 0
-it_p_VPS35 = 0
-it_p_DJ1 = 0
+PD_it_p_GBA1 = 0
+PD_it_p_LRRK2_mut = 0
+PD_it_p_VPS35 = 0
+PD_it_p_DJ1 = 0
 
 #THERAPEUTICS (BINARY)
-it_p_NPT200 = 0 #i.c.w. with all mutations
-it_p_DNL151 = 0 #i.c.w. LRRK2
-it_p_LAMP2A = 0 #i.c.w. with all mutations
+PD_it_p_NPT200 = 0 #i.c.w. with all mutations
+PD_it_p_DNL151 = 0 #i.c.w. LRRK2
+PD_it_p_LAMP2A = 0 #i.c.w. with all mutations
 
 #LEVEL OF CHOLESTEROL (1 OR 2X)
 chol_mp=300
@@ -27,7 +27,7 @@ cholSD = 0.1 # cholesterol module
 SD = 0.1
 DelaySD = 0.1 #calcium clock and MDVs
 
-collect_rate_analytics = ["yes","yes"] #first element consumption rate analytics, second element production rate analytics. i.e. ["no", "yes"], means don't collect consumption rate, but collect production rate analytics.
+PD_collect_rate_analytics = ["no","no"] #first element consumption rate analytics, second element production rate analytics. i.e. ["no", "yes"], means don't collect consumption rate, but collect production rate analytics.
 dont_collect = ["no","no"]
 CaSD = 0 #CaSD = 0 or breaks.
 
